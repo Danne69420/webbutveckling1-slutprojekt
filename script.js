d446a8970a9adb8a8570d9194c193ae752c6f172
Danne69420 <Daniel.kindvall05@gmail.com>
2023-05-30 13:06:04 +0200

    finish index.html
    Parrallax scrolling works, the sidenav menu works, and i have added in actual images and headers.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  //if the window is too small we want to just overlay the menu without pushing the content to the left, since this makes the text go off screen.
+  if(window.innerWidth < 1000){
+    document.getElementById("mySidenav").style.width = "200px";
+  }
+  else{
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";  
+  }
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
